76b7dbbd60032c441ad7e6585a243deec89433f8
Jason Xie <jasonssssshieh@users.noreply.github.com>
2016-11-10 19:36:17 -0500

    Create Time: O(m*n), Space O(m*n).cpp

---
+++ Time: O(m*n), Space O(m*n).cpp
@@ -0,0 +1,64 @@
+//
+//  main.cpp
+//  Mininum Cost Path DP
+//
+//  Created by Zijian Xie on 2016/11/10.
+//  Copyright © 2016年 Jason Xie. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+using namespace std;
+
+class MinCostPath{
+private:
+    vector<vector<int>> cost;
+public:
+    MinCostPath(vector<vector<int>>& cost){
+        this->cost = cost;
+    }
+    
+    int DP(){
+        if(cost.empty()) return 0;
+        int m = (int)cost.size();
+        int n = (int)cost[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0] = cost[0][0];
+        for(int i = 1; i < m; ++i){
+            dp[i][0] = dp[i-1][0] + cost[i][0];
+        }
+        for(int j = 1; j < n; ++j){
+            dp[0][j] = dp[0][j-1] + cost[0][j];
+        }
+        
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                dp[i][j] = cost[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        int i = m-1, j = n-1;
+        string res = "cost[" + to_string(i)+ "]" + "[" + to_string(j) + "]" + "(cost is " + to_string(cost[i][j]) + ") ";
+        
+        while(i || j){
+            if(i > 0 && dp[i][j] == dp[i-1][j] + cost[i][j]){
+                i--;
+            }else{
+                j--;
+            }
+            res += "<- cost[" + to_string(i)+ "]" + "[" + to_string(j) + "]" + "(cost is " + to_string(cost[i][j]) + ") ";
+        }
+        cout<<"The minimum cost path is: " <<res<<endl;
+        cout<<"The minimum amount of cost is: "<<dp[m-1][n-1]<<endl;
+        return dp[m-1][n-1];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<vector<int>> cost = {{1,3,5,8},{4,2,1,7},{4,3,2,3}};
+    MinCostPath* test = new MinCostPath(cost);
+    test->DP();
+    return 0;
+}
